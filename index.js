c4c37288050e6c5579435a50af61c6288a4de4a2
Uliana Tikhonycheva <131668438+UlianaTi@users.noreply.github.com>
2023-08-06 18:25:09 +0300

    create new repo

---
+++ index.js
@@ -0,0 +1,44 @@
+// 1
+let cleanerSalary = 40000;
+let teacherSalary = 22000;
+console.log(cleanerSalary + teacherSalary);
+
+// 2
+let city = 'Владивосток';
+let year = 2000;
+console.log(city + " " + year);
+
+// 3
+let timeImMessenger = 128;
+let timeForFoto = 254;
+let timeForReading = 137;
+let timeForWatching = 201;
+console.log((timeImMessenger + timeForFoto + timeForReading + timeForWatching)/60);
+
+// 4
+console.log(2**5);
+
+// 5
+console.log("Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!");
+
+// 6
+const greetings = "Привет";
+const userName = "Кот"
+console.log(greetings + ", " + userName);
+
+// 7
+let time;
+time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+time -=15;
+console.log('Немного сторих в соцсетях. Осталось минут: ' + time);
+time -=10
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву')
+
+// 8
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32)/1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту - это ${celsius} градусов по Цельсию`)
+
